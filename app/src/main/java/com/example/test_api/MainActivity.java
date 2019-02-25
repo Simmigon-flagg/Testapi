f455a818611dfeefbf212e64f50ce44e1914da79
Simmigon <simmigon.flagg@gmail.com>
2019-02-24 23:19:29 -0500

    get User by Id

--- app/src/main/java/com/example/test_api/MainActivity.java
+++ app/src/main/java/com/example/test_api/MainActivity.java
@@ -10,7 +10,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     JSONplaceholderAPI jsonplaceholderAPI;
